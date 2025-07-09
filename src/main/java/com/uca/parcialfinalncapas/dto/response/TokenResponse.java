26ea7f483e85e2f33d80099e775d7b46c8adb696
00071222 <00071222@uca.edu.sv>
2025-07-08 20:22:02 -0600

    Chore: security impl

---
+++ src/main/java/com/uca/parcialfinalncapas/dto/response/TokenResponse.java
@@ -0,0 +1,14 @@
+package com.uca.parcialfinalncapas.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenResponse {
+    private String accessToken;
+}
